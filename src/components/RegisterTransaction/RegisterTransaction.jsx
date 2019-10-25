8d04fe4f24587d506e480bf69ee25f1309efcd9b
Washington Soares <wsoaresfilho@hotmail.com.br>
2019-10-24 23:39:50 -0300

    Improving Transaction Form

--- src/components/RegisterTransaction/RegisterTransaction.jsx
+++ src/components/RegisterTransaction/RegisterTransaction.jsx
@@ -1,15 +1,27 @@
 import React, { PureComponent } from 'react';
+import classNames from 'classnames';
+import './register.css';
+
+const transactionType = {
+    CREDIT: 'credit',
+    DEBIT: 'debit',
+};
 
 class RegisterTransaction extends PureComponent {
     constructor(props) {
         super(props);
 
         this.formRef = React.createRef();
+        this.toggleRef = React.createRef();
         this.state = {
-            text: 'HOOOOO',
+            isCredit: true,
         };
 
         this.onSubmit = this.onSubmit.bind(this);
+        this.handleTransactionTypeClick = this.handleTransactionTypeClick.bind(
+            this
+        );
+        this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
     }
 
     onSubmit(event) {
@@ -19,22 +31,94 @@ class RegisterTransaction extends PureComponent {
         this.formRef.current.reset();
     }
 
+    handleTransactionTypeClick(event) {
+        const tranType = event.target.value;
+        const isCredit = tranType === transactionType.CREDIT;
+        this.setState({
+            isCredit,
+        });
+    }
+
+    handleTransitionEnd() {
+        const signalRotation = this.state.isCredit ? '' : '-';
+        this.toggleRef.current.style.transform = `rotateY(${signalRotation}15deg)`;
+        setTimeout(() => (this.toggleRef.current.style.transform = ''), 400);
+    }
+
+    getTypeText() {
+        return this.state.isCredit
+            ? transactionType.CREDIT
+            : transactionType.DEBIT;
+    }
+
     render() {
-        const { texts } = this.props;
+        const { isCredit } = this.state;
+        const typeText = this.getTypeText();
+        const typeClassname = classNames({
+            credit: isCredit,
+            debit: !isCredit,
+            'register-transaction__radio': true,
+        });
         return (
-            <div>
-                <form onSubmit={this.onSubmit} ref={this.formRef}>
-                    <input type='text' placeholder='Input text here' />
-                    <button type='submit'>Add Text</button>
+            <div className='register-transaction__container'>
+                <h1 className='register-transaction__title'>
+                    Register Transaction Form
+                </h1>
+                <form
+                    className='register-transaction__form'
+                    onSubmit={this.onSubmit}
+                    ref={this.formRef}
+                >
+                    <input
+                        required
+                        className='register-transaction__input'
+                        type='text'
+                        placeholder='Transaction description'
+                    />
+                    <input
+                        required
+                        className='register-transaction__input'
+                        type='number'
+                        min='0.00'
+                        step='0.01'
+                        placeholder='Transaction value'
+                    />
+
+                    <div className={typeClassname} ref={this.toggleRef}>
+                        <input
+                            onChange={this.handleTransactionTypeClick}
+                            type='radio'
+                            id='type-credit'
+                            name='type'
+                            value={transactionType.CREDIT}
+                            checked={isCredit}
+                        />
+                        <label id='type-credit__label' htmlFor='type-credit'>
+                            Credit
+                        </label>
+
+                        <input
+                            onChange={this.handleTransactionTypeClick}
+                            type='radio'
+                            id='type-debit'
+                            name='type'
+                            value={transactionType.DEBIT}
+                            checked={!isCredit}
+                        />
+                        <label id='type-debit__label' htmlFor='type-debit'>
+                            Debit
+                        </label>
+
+                        <div
+                            id='flap'
+                            onTransitionEnd={this.handleTransitionEnd}
+                        >
+                            <span className='content'>{typeText}</span>
+                        </div>
+                    </div>
+
+                    <button type='submit'>Add Transaction</button>
                 </form>
-                <div>
-                    <p>Here goes the texts:</p>
-                    <ul>
-                        {texts.map((text, index) => (
-                            <li key={`item-${index}`}>{text}</li>
-                        ))}
-                    </ul>
-                </div>
             </div>
         );
     }
